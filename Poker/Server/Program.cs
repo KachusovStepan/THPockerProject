ab03e1fcccaa2682d8fc6decbad356c8c7f128be
KachusovStepan <stepanurfu@gmail.com>
2020-04-25 01:46:35 +0500

    added server project

---
+++ Poker/Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Server
+{
+    class Program
+    {
+        private static void StartServer(string address) {
+            if (address[address.Length - 1] == '/')
+                address = address.Substring(0, address.Length - 1);
+            var uri = new Uri(string.Format("{0}/Service", address));
+            var serviceType = typeof(IContract);
+            var serviceHost = new ServiceHost(serviceType, uri);
+            var binding = new BasicHttpBinding();
+            serviceHost.AddServiceEndpoint(serviceType, binding, address);
+            serviceHost.Open();
+            Console.WriteLine("Press any key to finish");
+            Console.ReadKey();
+            serviceHost.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            StartServer("http://localhost:8000/");
+        }
+    }
+}
---
+++ Poker/Server/SimpleService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Server
+{
+    [ServiceContract]
+    interface IContract
+    {
+        [OperationContract]
+        bool Send(string msg, int address);
+    }
+
+    class SimpleService : IContract
+    {
+        public bool Send(string msg, int address) {
+            Console.WriteLine("Player {0} doing {1}", address, msg);
+            if (msg == "")
+                return false;
+            return true;
+        }
+    }
+}
