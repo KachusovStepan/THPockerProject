5855440412b7f2edbf45220df9ee83431f31e19e
KachusovStepan <stepanurfu@gmail.com>
2020-05-03 23:29:46 +0500

    added timer for syncronization SyncTimer (for asking servis in background thread for state information)

---
+++ Poker/Client/SyncTimer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+    class SyncTimer
+    {
+		public int Interval { get; set; }
+
+		public SyncTimer() {
+			Interval = 300;
+		}
+
+		public SyncTimer(int interval) {
+			if (interval <= 0) {
+				throw new InvalidOperationException();
+			}
+			Interval = interval;
+		}
+
+		public event Action<int> Tick;
+
+		public void Start() {
+			for (int i = 0; ; i++) {
+				if (Tick != null) {
+					Tick(i);
+				}
+
+				Thread.Sleep(Interval);
+			}
+		}
+	}
+}
